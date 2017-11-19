e1c24d265b14c34cbb3d4a2e67c7e34e116ee51a
arahuman@gmail.com <arahuman@gmail.com>
2017-11-19 17:51:24 -0600

    projects page addition

--- server.js
+++ server.js
@@ -1,18 +1,22 @@
 const express = require('express');
 const hbs = require('hbs');
 const port = process.env.PORT || 3000;
+const fs = require('fs');
 
 hbs.registerPartials(__dirname  + '/views/partials');
 hbs.registerHelper('getCurrentYear',()=>{
   return new Date().getFullYear();
 });
-var app = express();
 
+var app = express();
 app.set('view engine','hbs');
 app.use(express.static(__dirname + '/public'));
+
 app.use((req,res,next)=>{
   var now = new Date().toString();
-  console.log(`${now}: ${req.method} - ${req.url}`)
+  var log = `${now}: ${req.method} - ${req.url}`
+  fs.appendFile('server.log',log+"\n");
+  console.log(log)
   next();
 });
 
@@ -33,11 +37,11 @@ app.get('/about', (req, res) => {
   });
 });
 
-app.get('/bad', (req, res) => {
-  res.send({
-    errorMessage: 'Unable to handle request'
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle:'Projects Page'
   });
-});
+})
 
 app.listen(port, () => {
   console.log(`Server is up on port ${port}`);
